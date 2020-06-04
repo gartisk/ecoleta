fb652bb1999d8b1c8dd36d5bdb0e1c876a5d6437
Guilherme Araujo <gartisk@gmail.com>
2020-06-03 21:23:27 -0300

    Day 2: Principais Conceitos - Rest

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,38 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const users = [
+    "Guilherme",
+    "Sandro",
+    "Gabriel",
+    "Victor"
+];
 
 app.get('/users', (request, response) => {
     console.log('listagem de usuários');
+    const search = String(request.query.search);
+    console.log(search);
 
-    response.json([
-        "Guilherme"
-    ]);
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    return response.json(users[Number(request.params.id)]);
+});
+
+app.post('/users', (request, response) => {
+    console.log('listagem de usuários3');
+    const data = request.body;
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
