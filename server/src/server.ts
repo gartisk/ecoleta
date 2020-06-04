f38404504b5511c231c412b5d22384290d6a05bb
Guilherme Araujo <gartisk@gmail.com>
2020-06-03 21:33:48 -0300

    Day 2: Hello W, new Routes, apagad metodos antigos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
-
-const users = [
-    "Guilherme",
-    "Sandro",
-    "Gabriel",
-    "Victor"
-];
-
-app.get('/users', (request, response) => {
-    console.log('listagem de usuários');
-    const search = String(request.query.search);
-    console.log(search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    return response.json(users[Number(request.params.id)]);
-});
-
-app.post('/users', (request, response) => {
-    console.log('listagem de usuários3');
-    const data = request.body;
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
