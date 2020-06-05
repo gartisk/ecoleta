029132c863d40d888713a9a7e4c8284e93a12485
Guilherme Araujo <gartisk@gmail.com>
2020-06-05 02:08:58 -0300

    Day 2: Separação por controllers point e items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,65 +1,23 @@
 import express from 'express';
 import knex from './database/connection';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
+// Padrao da comunidade: index, show, create, update, delete
 
-    const serializedItems = items.map(item =>{
-        return {
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`
-        }
-    });
+const pointsController = new PointsController;
+const itemsController = new ItemsController;
 
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
 
-    const trx = await knex.transaction();
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show);
 
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    });
 
-    const point_id = insertedIds[0];
-    console.log(point_id);
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id
-        };
-    });
-
-    console.log(pointItems);
-
-    var idPi = await trx('point_items').insert(pointItems);
-
-    console.log(idPi);
-
-    await trx.commit();
-    
-    return response.json({ success: true });
-});
 
 export default routes;
