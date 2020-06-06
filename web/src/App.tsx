6a569154a43ba34ad920c4607d3011488e647e87
Guilherme Araujo <gartisk@gmail.com>
2020-06-06 18:01:04 -0300

    Day 3: Retornado a aplicação antes do exemplo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-  function handleButtonClick(){
-    setCounter( counter +1 );
-  }
-
   return (
-    <div>
-      <Header title="ola"></Header>
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar </button>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
