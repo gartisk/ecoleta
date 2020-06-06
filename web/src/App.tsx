58ca5bb9180bc71437687ed948326088e2300809
Guilherme Araujo <gartisk@gmail.com>
2020-06-06 20:18:54 -0300

    Day 3: Construção da navegação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home';
+import Routes from './routes';
+import CreatePoint from './pages/CreatePoint';
 
 function App() {
   return (
-    <Home />
+    <Routes />
   );
 }
 
