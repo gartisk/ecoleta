2b751e4c414003c279b71695bdee5da4925cccc5
Guilherme Araujo <gartisk@gmail.com>
2020-06-07 01:24:55 -0300

    Day 3: Buscando totos os itens de coleta da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 import './styles.css';
 
 import logo  from '../../assets/logo.svg';
 
+
+// sempre que se cria um estado para um array ou objeto é necessário informar o tipo deste.
+
+interface Item {
+    id: number,
+    title: string,
+    image_url: string,
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() =>  {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -52,6 +70,14 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+
+                    <Map center={[-22.9132525, -43.7261806]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-22.9132525, -43.7261806]} />
+                    </Map>
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
@@ -76,30 +102,12 @@ const CreatePoint = () => {
                     </legend>
                 </fieldset>
                 <ul className="items-grid">
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
-                    <li className="selected">
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
-                    <li>
-                        <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                        <span>Oléo de cozinha</span>
-                    </li>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            <img src={item.image_url} alt={item.title}/>
+                            <span>{item.title}</span>
+                        </li>
+                    ))};
                 </ul>
                 <button type="submit">
                     Cadastrar ponto de coleta
